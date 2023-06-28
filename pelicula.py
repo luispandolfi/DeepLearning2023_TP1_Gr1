8e4c85af3bae0efe26a021b5a8db7ea724e88ab3
Luis Pandolfi <lpandolfi@infragistics.com>
2023-06-28 20:07:59 -0300

    definiciones metodos faltantes

--- pelicula.py
+++ pelicula.py
@@ -84,3 +84,21 @@ class Pelicula:
     df = df.dropna()
     df["Release Date"] = pd.to_datetime(df["Release Date"], format="%d-%b-%Y")
     return df
+  
+def get_stats(cls,df_mov, anios=None, generos=None):
+  # Este class method imprime una serie de estadísticas calculadas sobre
+  # los resultados de una consulta al DataFrame df_mov. 
+  # Las estadísticas se realizarán sobre las filas que cumplan con los requisitos de:
+  # anios: [desde_año, hasta_año]
+  # generos: [generos]
+  # Las estadísticas son:
+  # - Datos película más vieja
+  # - Datos película más nueva
+  # - Bar plots con la cantidad de películas por año/género.
+  pass
+
+def remove_from_df(self, df_mov):
+  # Borra del DataFrame el objeto contenido en esta clase.
+  # Para realizar el borrado todas las propiedades del objeto deben coincidir
+  # con la entrada en el DF. Caso contrario imprime un error.
+  pass
