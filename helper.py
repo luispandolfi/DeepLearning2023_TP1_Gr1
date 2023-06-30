4decada772ef4a963fd74af851d6ad1e85032fa6
Luis Pandolfi <lpandolfi@infragistics.com>
2023-06-30 08:02:56 -0300

    Clase DataFrameStore que contiene todos los dataframes y nos permite accederlos por su nombre. Evitando trabajar con la tupla de dataframes.

--- helper.py
+++ helper.py
@@ -11,3 +11,17 @@ def load_all(file_personas, file_trabajadores, file_usuarios, file_peliculas, fi
   df_peliculas = Pelicula.create_df_from_csv(file_peliculas)
   df_scores = Score.create_df_from_csv(file_scores, df_usuarios, df_peliculas)
   return df_personas, df_trabajadores, df_usuarios, df_peliculas, df_scores
+
+def load_to_dfstore(file_personas, file_trabajadores, file_usuarios, file_peliculas, file_scores):
+  dfs = load_all(file_personas, file_trabajadores, file_usuarios, file_peliculas, file_scores)
+  return DataFrameStore(dfs[0], dfs[1], dfs[2], dfs[3], dfs[4])
+
+
+class DataFrameStore:
+
+  def __init__(self, personas, trabajadores, usuarios, peliculas, scores):
+    self.personas = personas
+    self.trabajadores = trabajadores
+    self.usuarios = usuarios
+    self.peliculas = peliculas
+    self.scores = scores
