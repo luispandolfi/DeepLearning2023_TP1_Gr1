6b4bb5cc532c94f29190f694e006b3a2cb4316d4
Luis Pandolfi <lpandolfi@infragistics.com>
2023-07-04 09:43:50 -0300

    Ajuste de espacios, renombe de df_mov a df

--- pelicula.py
+++ pelicula.py
@@ -67,33 +67,33 @@ class Pelicula:
       lista_respuesta.append(peliculax)
     return lista_respuesta
   
-  def write_df(self, df_mov): 
+  def write_df(self, df): 
     # Este método recibe el dataframe de películas y agrega la película
     # Si el id es None, toma el id más alto del DF y le suma uno. Si el 
     # id ya existe, no la agrega y devuelve un error.
-    new_row={
+    new_row = {
       "name": self.nombre,
-      "Release Date": self.anio,
+      "Release Date": self.fecha_estreno,
     }
     
-    if self.id==None:
-      new_row["id"]=df_mov.id.max() +1
-    elif self.id in df_mov.id.values:
+    if self.id == None:
+      new_row["id"] = df.id.max() + 1
+    elif self.id in df.id.values:
       ex = ValueError()
-      ex.strerror="Id no válido, ya se encuentra en el DataFrame"
+      ex.strerror = "Id no válido, ya se encuentra en el DataFrame"
       raise ex
     else:
-      new_row["id"]=self.id
+      new_row["id"] = self.id
       
-    for key in df_mov.keys()[-19:]:
+    for key in df.keys()[-19:]:
       if key in self.generos:
-        new_row[f"{key}"]=1
+        new_row[f"{key}"] = 1
       else:
-        new_row[f"{key}"]=0
+        new_row[f"{key}"] = 0
 
     df_dictionary = pd.DataFrame([new_row])
-    df_mov = pd.concat([df_mov, df_dictionary], ignore_index=True)
-    return df_mov
+    df = pd.concat([df, df_dictionary], ignore_index=True)
+    return df
   
   def remove_from_df(self, df):
     # Borra del DataFrame el objeto contenido en esta clase.
