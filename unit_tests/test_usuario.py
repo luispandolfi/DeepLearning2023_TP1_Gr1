c0a9d34183fa8694bb22314acf13e166063cd854
Luis Pandolfi <lpandolfi@infragistics.com>
2023-07-05 22:50:34 -0300

    fix test

--- unit_tests/test_usuario.py
+++ unit_tests/test_usuario.py
@@ -1,10 +1,12 @@
 import pytest
+from persona import Persona
 from usuario import Usuario
 
 
 @pytest.fixture(scope="class")
 def df_usuarios():
-  return Usuario.create_df_from_csv("../csv_files/usuarios.csv")
+  df_personas = Persona.create_df_from_csv("./csv_files/personas.csv")
+  return Usuario.create_df_from_csv("./csv_files/usuarios.csv", df_personas)
 
 class TestUsuario:
 
@@ -14,4 +16,4 @@ class TestUsuario:
         usuario = usuarios[0]
         assert usuario.id == 17
         assert usuario.ocupacion == "programmer"
-        assert usuario.fecha_alta == "1998-01-18 23:20:19"
+        assert usuario.fecha_alta.strftime("%Y-%m-%d %H:%M:%S") == "1998-01-18 23:20:19"
