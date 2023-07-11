b220c90440178295aa388b58e3207a05958369cf
Luis Pandolfi <luispandolfi@gmail.com>
2023-07-11 00:03:48 -0300

    Agrego tests para el write de usuario, y dejo algunos TODO con casos para revisar

--- unit_tests/test_usuario.py
+++ unit_tests/test_usuario.py
@@ -12,21 +12,50 @@ def df_usuarios():
 
 class TestUsuario:
 
-    def test_get_por_id(self, df_usuarios):
-        usuarios = Usuario.get_from_df(df_usuarios, 17)
-        assert len(usuarios) == 1
-        usuario = usuarios[0]
-        assert usuario.id == 17
-        assert usuario.ocupacion == "programmer"
-        assert usuario.fecha_alta.strftime("%Y-%m-%d %H:%M:%S") == "1998-01-18 23:20:19"
+  # TODO Revisar este caso, agregar un usuario con id None no deberia permitirse,
+  # pues no hay forma de verificar que exista en personas
+  def test_write_id_none(self, df_usuarios):
+    cantidad_inicial = len(df_usuarios.id)
+    usuario = Usuario(None, datetime(2023, 7, 9), "student")
+    df = usuario.write_df(df_usuarios)
+    # el df original queda incambiado
+    assert len(df_usuarios.id) == cantidad_inicial
+    # la persona agregada se almacena en el df resultado
+    assert len(df.id) == cantidad_inicial + 1
 
 
-    def test_get_por_fecha(self, df_usuarios):
-        fecha1 = datetime(1998, 1, 6, 0, 0, 0)
-        fecha2 = datetime(1998, 1, 6, 23, 59, 59)
-        usuarios = Usuario.get_from_df(df_usuarios, None, [fecha1, fecha2])
-        assert len(usuarios) == 10
-        for usuario in usuarios:
-            assert usuario.fecha_alta.year == 1998
-            assert usuario.fecha_alta.month == 1
-            assert usuario.fecha_alta.day == 6
+  # TODO Revisar este caso, el write debe verificar que el id del usuario exista en personas
+  def test_write_id_no_existente(self, df_usuarios):
+    cantidad_inicial = len(df_usuarios.id)
+    usuario = Usuario(None, datetime(2023, 7, 9), "student")
+    df = usuario.write_df(df_usuarios)
+    # el df original queda incambiado
+    assert len(df_usuarios.id) == cantidad_inicial
+    # la persona agregada se almacena en el df resultado
+    assert len(df.id) == cantidad_inicial + 1
+
+
+  def test_write_id_existente(self, df_usuarios):
+    usuario = Usuario(11, datetime(2023, 7, 9), "student")
+    with pytest.raises(ValueError):
+      df = usuario.write_df(df_usuarios)
+
+
+  def test_get_por_id(self, df_usuarios):
+    usuarios = Usuario.get_from_df(df_usuarios, 17)
+    assert len(usuarios) == 1
+    usuario = usuarios[0]
+    assert usuario.id == 17
+    assert usuario.ocupacion == "programmer"
+    assert usuario.fecha_alta.strftime("%Y-%m-%d %H:%M:%S") == "1998-01-18 23:20:19"
+
+
+  def test_get_por_fecha(self, df_usuarios):
+    fecha1 = datetime(1998, 1, 6, 0, 0, 0)
+    fecha2 = datetime(1998, 1, 6, 23, 59, 59)
+    usuarios = Usuario.get_from_df(df_usuarios, None, [fecha1, fecha2])
+    assert len(usuarios) == 10
+    for usuario in usuarios:
+      assert usuario.fecha_alta.year == 1998
+      assert usuario.fecha_alta.month == 1
+      assert usuario.fecha_alta.day == 6
