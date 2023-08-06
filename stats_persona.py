be52b6c22b1c38c6674c9e2ae3bcec1d98fea397
Luis Pandolfi <luispandolfi@gmail.com>
2023-08-06 13:09:06 -0300

    titulos en las charts de persona

--- stats_persona.py
+++ stats_persona.py
@@ -8,6 +8,9 @@ class StatsPersona:
         stats_personas = Persona.get_stats(df_personas)
         personas_por_anio = stats_personas["personas_por_anio"]
         plt.bar(personas_por_anio.index, personas_por_anio.values)
+        plt.xlabel("Año de nacimiento")
+        plt.ylabel("#Personas")
+        plt.title("Cantidad de personas por año de nacimiento")
         plt.show()
 
 
@@ -16,6 +19,9 @@ class StatsPersona:
         stats_personas = Persona.get_stats(df_personas)
         personas_por_genero = stats_personas["personas_por_genero"]
         plt.bar(personas_por_genero.index, personas_por_genero.values)
+        plt.xlabel("Género")
+        plt.ylabel("#Personas")
+        plt.title("Cantidad de personas por género")
         plt.show()
         
 
